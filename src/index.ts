2584957552524ff9e2a9b0dbee64595dd17cfc42
yulikafsd <ju.zagorovsky@gmail.com>
2026-06-26 22:09:35 +0200

    add .js to import

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { concatenation } from './concatenation';
+import { concatenation } from './concatenation.js';
 
 const button = document.querySelector('button')!;
 const input = document.querySelector('input')!;
